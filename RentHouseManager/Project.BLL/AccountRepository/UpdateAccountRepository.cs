1de4e9dd7fbda67b7f61b9fa916cf7e2f4df8668
Trần Thành Phương Đăng <phuongdang012@gmail.com>
2018-05-05 03:18:20 +0700

    Fix db and add UpdateAccount

--- RentHouseManager/Project.BLL/AccountRepository/UpdateAccountRepository.cs
+++ RentHouseManager/Project.BLL/AccountRepository/UpdateAccountRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RentHouseManager.Project.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,34 @@ namespace RentHouseManager.Project.BLL.AccountRepository
 {
     public class UpdateAccountRepository
     {
-
+        private RentHouseManagerEntities _context;
+        public UpdateAccountRepository()
+        {
+            _context = new RentHouseManagerEntities();
+        }
+        public bool UpdateAccountByName(ACCOUNT newAccount)
+        {
+            bool result;
+            var searchAccount = _context.ACCOUNTs.FirstOrDefault(acc => acc.IDUSER == newAccount.IDUSER);
+            if (searchAccount == null)
+            {
+                result = false;
+            }
+            else
+            {
+                try
+                {
+                    searchAccount = newAccount;
+                    _context.SaveChanges();
+                    result = true;
+                }
+                catch (Exception e)
+                {
+                    result = false;
+                    throw e;
+                }
+            }
+            return result;
+        }
     }
 }
