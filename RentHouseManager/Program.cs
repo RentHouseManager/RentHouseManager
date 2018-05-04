cc925a5dc49f0a9f77f641f1712fce9dd054e15b
DESKTOP-TU3D1ST\SONY <kd230997@gmail.com>
2018-05-04 13:21:50 +0700

    Fix conflict

--- RentHouseManager/Program.cs
+++ RentHouseManager/Program.cs
@@ -17,16 +17,7 @@ namespace RentHouseManager
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-<<<<<<< HEAD
             Application.Run(new Formlogin());
-=======
-<<<<<<< HEAD
-           // Application.Run(new Formlogin());
-
-=======
-            Application.Run(new formHeThong());
->>>>>>> 646c948848e6fe55c4128743337f6d917c7c1202
->>>>>>> c83b23d399c68e407fa7fc8733a6b9962a9f5a63
         }
     }
 }
